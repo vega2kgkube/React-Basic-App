fc0064ef78e12782ef44dea6c0fc167552ba8009
vega2kgkube <vega2kgkube@gmail.com>
2025-09-01 09:53:03 +0900

    Proptypes

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
     render() {
@@ -15,5 +16,8 @@ class MyComponent extends Component {
 }
 MyComponent.defaultProps = {
     name: '리액트JS'
+};
+MyComponent.propTypes = {
+    name: PropTypes.string
 };
 export default MyComponent;
